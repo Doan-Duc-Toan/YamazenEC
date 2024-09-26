40fd4590193bec1464bdfc0239ae0bcfac064446
yamada5131 <son.nl215131@gmail.com>
2024-09-26 07:26:28 +0700

    Add img url column to products table and product factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace Database\Factories;
+
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
 
 class ProductFactory extends Factory
 {
@@ -13,12 +13,14 @@ class ProductFactory extends Factory
      * @return array<string, mixed>
      */
     protected $model = Product::class;
+
     public function definition(): array
     {
         return [
             'name' => $this->faker->word,
             'description' => $this->faker->sentence,
             'origin' => $this->faker->country,
+            'img_url' => $this->faker->imageUrl(),
             'created_at' => now(),
             'updated_at' => now(),
         ];
